3f925fa97e88912656567c0c41ecb34ccf2da6df
aryan <aryan.jand@gmail.com>
2023-11-22 10:49:09 -0800

    Changing model

--- src/open-ai/open-ai.service.ts
+++ src/open-ai/open-ai.service.ts
@@ -14,7 +14,7 @@ export class OpenAiService {
       console.log(openai);
 
       const response = await openai.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-3.5-turbo',
         messages: [
           {
             role: 'system',
