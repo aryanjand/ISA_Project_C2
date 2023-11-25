2e970a9e633e8a4b4a89b0d55f7bc8ff45c4c21d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 15:34:53 -0800

    Fixed the getAllStories

--- src/story/story.service.ts
+++ src/story/story.service.ts
@@ -3,14 +3,17 @@ import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { ValidationException } from '../common';
 import { Request } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class StoryService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
-  async getAllStories(user: User) {
+  async getAllStories(token: string) {
     try {
-      if (user.user_privilege != 'ADMIN') {
+      const {user} = await this.jwt.verifyAsync(token);
+      console.log(user);
+      if (user.user_privilege !== 'ADMIN') {
         throw new ValidationException('Something went wrong');
       }
       const story = await this.prisma.story.findMany();
