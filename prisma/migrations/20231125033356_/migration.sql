970ac677d32c72be00c4d4b126ad463c42a5f05f
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 17:53:54 -0800

    Added requests table

--- prisma/migrations/20231125033356_/migration.sql
+++ prisma/migrations/20231125033356_/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE `User` (
     `password` VARCHAR(255) NOT NULL,
     `user_privilege` ENUM('ADMIN', 'USER') NOT NULL DEFAULT 'USER',
     `api_calls_left` INTEGER NOT NULL DEFAULT 20,
+    `total_requests` INTEGER NOT NULL DEFAULT 0,
 
     UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
@@ -35,5 +36,17 @@ CREATE TABLE `ExpiredJwt` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Requests` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `method` VARCHAR(15) NOT NULL,
+    `endpoint` VARCHAR(50) NOT NULL,
+    `total` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
 -- AddForeignKey
 ALTER TABLE `Story` ADD CONSTRAINT `Story_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
