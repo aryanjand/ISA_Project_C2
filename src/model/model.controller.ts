00b12481b8072ac0344dda9990bfd210fbba4ef0
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 21:55:36 -0800

    Raised a 405 status when user has no more api_calls_left

--- src/model/model.controller.ts
+++ src/model/model.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, Query, Request as Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Query,
+  Request as Req,
+} from '@nestjs/common';
 import { ApiQuery } from '@nestjs/swagger';
 import { ModelService } from './model.service';
 import { OpenAiService } from '../open-ai/open-ai.service';
-import {Request} from 'express';
+import { Request } from 'express';
 import { RequestsService } from 'src/requests/requests.service';
 import { UserService } from 'src/user/user.service';
 import { MODAL_MESSAGES } from './modal.constants';
@@ -25,14 +31,18 @@ export class ModelController {
   async generateTokens(
     @Req() request: Request,
     @Query('description') description: string,
-  ): Promise<{prompt: string}> {
+  ): Promise<{ prompt: string }> {
     const user = await this.modelService.getUser(request.cookies.token);
     if (user.api_calls_left <= 0) {
-      return;
+      throw new HttpException('No more API Calls left!', 405);
     }
     const tokens = await this.modelService.identifyTokens(description);
     const generatedText = await this.openaiService.openAiResponse(tokens);
-    const success = await this.modelService.storeStory(user, generatedText, description);
+    const success = await this.modelService.storeStory(
+      user,
+      generatedText,
+      description,
+    );
     this.requestService.incrementRequest('/model/GenerateStory', 'GET');
     this.userService.incrementTotalRequests(request.cookies.token);
     if (success) {
